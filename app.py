ccaccdf86e7927e7f19bd2f0e27a1fe47ac89d79
HANEEF <erpsysware.com>
2021-12-01 21:35:56 +0700

    dsedsdsdsddfxc

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iaqqzyhiqpcumq:d7a472e0ec5c4c4c5d633e50d2eff7cb1c3d3afa1262aa0902f7f29c76e0c8ee@ec2-44-198-211-34.compute-1.amazonaws.com:5432/dd8crl0af2jhnd'
-# db =SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://iaqqzyhiqpcumq:d7a472e0ec5c4c4c5d633e50d2eff7cb1c3d3afa1262aa0902f7f29c76e0c8ee@ec2-44-198-211-34.compute-1.amazonaws.com:5432/dd8crl0af2jhnd'
+db =SQLAlchemy(app)
 
 @app.route('/')
 def hello():
