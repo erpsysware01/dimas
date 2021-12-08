1a1e18e7060f2ca71f6b914a12ddbc94b3f5efd0
HANEEF <erpsysware.com>
2021-12-09 06:15:36 +0700

    hnghd

--- app.py
+++ app.py
@@ -77,6 +77,6 @@ if __name__== '__main__':
   app.register_blueprint(route_masjid)
   app.register_blueprint(route_user)
   # db.create_all()
-  app.run(debug=True)
-  # app.run()
+  # app.run(debug=True)
+  app.run()
 
