7988b912ed6fe7a8564c4be09e67d8abf51c0f6c
HANEEF <erpsysware.com>
2021-12-09 23:27:36 +0700

    zscfsdxdsf

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 # from flask_script import Manager
 from flask_migrate import  Migrate
-from route_test import app1
+# from route_test import app1
 # from route_masjid import route_masjid
 
 
